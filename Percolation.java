ffdaacd4e7a4e4d79437db1bddc29e5d98991c25
Oleksandr Korobov <oleksandr.korobov@scandinavianhouse.no>
2014-06-21 00:30:56 +0300

    Percollation started.

---
+++ Percolation.java
@@ -0,0 +1,26 @@
+public class Percolation {
+   
+    // create N-by-N grid, with all sites blocked
+    public Percolation(int N) {
+    
+    }
+    
+    // open site (row i, column j) if it is not already
+    public void open(int i, int j) {
+    
+    }
+    // is site (row i, column j) open?
+    public boolean isOpen(int i, int j) {
+        return false;
+    } 
+    
+    // is site (row i, column j) full?    
+    public boolean isFull(int i, int j) {
+        return false;
+    }
+    
+    // does the system percolate?
+        public boolean percolates() {
+            return false;
+    }
+}
