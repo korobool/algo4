dddef7d088a3a090af438cfd386a26fe08a664a1
Oleksandr Korobov <oleksandr.korobov@scandinavianhouse.no>
2014-06-21 09:32:02 +0300

    Indexing model updated.

--- Percolation.java
+++ Percolation.java
@@ -9,6 +9,10 @@ public class Percolation {
     private Cell[][] cells;
     
     private WeightedQuickUnionUF unionFind;
+    
+    private void connectToOpenNeighbors(int i, int j) {
+    	
+    }
 	
     // create N-by-N grid, with all sites blocked
     public Percolation(int N) {
@@ -24,7 +28,14 @@ public class Percolation {
     
     // open site (row i, column j) if it is not already
     public void open(int i, int j) {
-    
+    	if (i < 1 || i > cells.length || j < 1 || j > cells.length) {
+            throw new java.lang.IndexOutOfBoundsException();
+        }
+    	
+    	if (!isOpen(i-1, j-1)) {
+    		cells[i-1][j-1] = Cell.OPEN;
+    		connectToOpenNeighbors(i, j);
+    	}
     }
     
     // is site (row i, column j) open?
@@ -32,7 +43,7 @@ public class Percolation {
     	if (i < 1 || i > cells.length || j < 1 || j > cells.length) {
             throw new java.lang.IndexOutOfBoundsException();
         }
-        return cells[i][i] == Cell.OPEN;
+        return cells[i-1][j-1] == Cell.OPEN;
     } 
     
     // is site (row i, column j) full?    
@@ -40,11 +51,11 @@ public class Percolation {
     	if (i < 1 || i > cells.length || j < 1 || j > cells.length) {
             throw new java.lang.IndexOutOfBoundsException();
         }
-    	return cells[i][i] == Cell.LIQUID;
+    	return cells[i-1][j-1] == Cell.LIQUID;
     }
     
     // does the system percolate?
         public boolean percolates() {
-            return false;
+        	return unionFind.connected(...);
     }
 }
