abf4534d4d6497bcb12d6733770c6b4ea961b7eb
Oleksandr Korobov <oleksandr.korobov@scandinavianhouse.no>
2014-06-21 10:26:09 +0300

    General functionality draft for Stats.

--- PercolationStats.java
+++ PercolationStats.java
@@ -1,31 +1,37 @@
 public class PercolationStats {
 	
+	private double[] values;
+	private int t = 0;
+	
 	// perform T independent computational experiments on an N-by-N grid
     public PercolationStats(int N, int T) {
         if (N <= 0 || T <= 0) {
             throw new IllegalArgumentException();
         }
+        
+        t = T;
     }
     
 	// sample mean of percolation threshold 
     public double mean() {
-        return 0;
+    	return StdStats.mean(values);
     } 
     
 	// sample standard deviation of percolation threshold 
     public double stddev() {
-        return 0;
+    	return StdStats.stddev(values);
     }
     
 	// returns lower bound of the 95% confidence interval
-    
     public double confidenceLo() {
-        return 0;
+    	double mean = mean();
+    	return mean - ((1.96 * stddev()) / Math.sqrt(t));
     }
     
     // returns upper bound of the 95% confidence interval
     public double confidenceHi() {
-        return 0;
+    	double mean = mean();
+    	return mean - ((1.96 * stddev()) / Math.sqrt(t));
     }
    
     // test client, described below
@@ -34,15 +40,8 @@ public class PercolationStats {
         int T = Integer.parseInt(args[1]);
         PercolationStats stats = new PercolationStats(N, T);
         
-        double mean = stats.mean();
-        double stddev = stats.stddev();
-        double[] interval = new double[2];
-        
-        interval[0] = mean - ((1.96 * stddev) / Math.sqrt(T));
-        interval[1] = mean + ((1.96 * stddev) / Math.sqrt(T));
-        
-        System.out.println("mean                    = " + mean);
-        System.out.println("stddev                  = " + stddev);
-        System.out.println("95% confidence interval = " + interval[0] + ", " + interval[1]);
+        System.out.println("mean                    = " + stats.mean());
+        System.out.println("stddev                  = " + stats.stddev());
+        System.out.println("95% confidence interval = " + stats.confidenceLo() + ", " + stats.confidenceHi());
     }
 }
