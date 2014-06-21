0fa2b4dd24079340ede62f8ed333188bf94f73a6
Oleksandr Korobov <oleksandr.korobov@scandinavianhouse.no>
2014-06-21 08:54:32 +0300

    Constructor implemented

--- Percolation.java
+++ Percolation.java
@@ -1,14 +1,32 @@
 public class Percolation {
    
+	private enum Cell {
+        CLOSED,
+        OPEN,
+        LIQUID
+    }
+	
+    private Cell[][] cells;
+    
+    private WeightedQuickUnionUF unionFind;
+	
     // create N-by-N grid, with all sites blocked
     public Percolation(int N) {
-    
+        cells     = new Cell[N][N];
+        unionFind = new WeightedQuickUnionUF(N * N + 2);
+        
+        for (int r = 0; r < N; r++) {
+            for (int c = 0; c < N; c++) {
+                cells[r][c] = Cell.CLOSED;
+            }
+        }
     }
     
     // open site (row i, column j) if it is not already
     public void open(int i, int j) {
     
     }
+    
     // is site (row i, column j) open?
     public boolean isOpen(int i, int j) {
         return false;
