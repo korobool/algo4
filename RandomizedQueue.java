9a79ac3a66f1e115371d6b86e52497cd13233db8
Dmytro Novikov <dimetrios@gmail.com>
2014-07-06 12:22:30 +0300

    Initial RandomizedQueue added.

---
+++ RandomizedQueue.java
@@ -0,0 +1,34 @@
+public class RandomizedQueue<Item> implements Iterable<Item> {
+    
+    // construct an empty randomized queue
+    public RandomizedQueue() {
+    }
+    
+    // is the queue empty?
+    public boolean isEmpty() {
+    }
+
+    // return the number of items on the queue
+    public int size() {
+    }
+
+    // add the item
+    public void enqueue(Item item) {
+    }
+
+    // delete and return a random item
+    public Item dequeue() {
+    }
+
+    // return (but do not delete) a random item
+    public Item sample() {
+    }
+
+    // return an independent iterator over items in random order
+    public Iterator<Item> iterator() {
+    }
+
+    // unit testing
+    public static void main(String[] args) {
+    }
+}
